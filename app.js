b67e912f6d92f74213d0c92e13fd4813abb8a064
AsdWaring <a.s.d.waring@gmail.com>
2022-08-06 16:28:55 -0700

    formatting

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-// create script for user input 
+// create script for user input
 // create script for computer input
 // compare user input against computer input
 // write result
 // loop five
-var user = prompt ()
-var comp = 'rock'
+
+var user = prompt();
+var comp = "rock";
 function game(user, comp) {
-    if (user == comp)
-        alert('you tie')
-    else if (user == 'rock' && comp == 'paper')
-        alert('you lose')
-    else if (user == 'rock' && comp == 'scissors')
-        alert('you win')
-    else if (user == 'paper' && comp == 'rock')
-        alert('you win')
-    else if (user == 'paper' && comp == 'scissors')
-        alert('you lose')
-    else if (user == 'scissors' && comp == 'rock')
-        alert('you lose')
-    else if (user == 'scissors' && comp == 'paper')
-        alert('you win')
+  if (user == comp) alert("you tie");
+  else if (user == "rock" && comp == "paper") alert("you lose");
+  else if (user == "rock" && comp == "scissors") alert("you win");
+  else if (user == "paper" && comp == "rock") alert("you win");
+  else if (user == "paper" && comp == "scissors") alert("you lose");
+  else if (user == "scissors" && comp == "rock") alert("you lose");
+  else if (user == "scissors" && comp == "paper") alert("you win");
 }
 
-game(user, comp)
+game(user, comp);
