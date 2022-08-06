ef3807f3b3685fe76883ec07b87d1268fdd41452
AsdWaring <a.s.d.waring@gmail.com>
2022-08-06 10:24:37 -0700

    worked on initial logic for game

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 // compare user input against computer input
 // write result
 // loop five
+const user = button 
+function game(user, comp) {
+    if (user == comp)
+        alert('you tie')
+    else if (user == 'rock' && comp == 'paper')
+        alert('you lose')
+    else if (user == 'rock' && comp == 'scissors')
+        alert('you win')
+    else if (user == 'paper' && comp == 'rock')
+        alert('you win')
+    else if (user == 'paper' && comp == 'scissors')
+        alert('you lose')
+    else if (user == 'scissors' && comp == 'rock')
+        alert('you lose')
+    else if (user == 'scissors' && comp == 'paper')
+        alert('you win')
+}
+
+game('','paper')
