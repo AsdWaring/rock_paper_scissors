9570835c6dd17d1ad085dc74d47fc4d8613efe7e
AsdWaring <a.s.d.waring@gmail.com>
2022-08-08 10:05:01 -0700

    progress made on player select

--- random.js
+++ random.js
@@ -1,25 +1,51 @@
+/* I am making good progress, but I need to understand
+how to start the game with the the selection of the user buttons
+I am learning something new each time I tackle this problem 
+player selection may need to be converted into a function?*/
+
 // setting constants
-//
-const comp = computerselect;
+const rockbutton = document.querySelector("#rock");
+const paperbutton = document.querySelector("#paper");
+const scissorsbutton = document.querySelector("#scissors");
+// player selection
+rockbutton.addEventListener("click", () => {
+  console.log("user: rock");
+  user = "rock";
+});
+
+paperbutton.addEventListener("click", () => {
+  console.log("user: paper");
+  user = "paper";
+});
+
+scissorsbutton.addEventListener("click", () => {
+  console.log("user: scissors");
+  user = "scissors";
+});
 //random select
 //
+
 function computerselect() {
   randomnumber = Math.floor(Math.random() * 3);
   if (randomnumber == "0") {
-    return "rock";
+    console.log("Computer: rock");
+    comp = "rock";
   } else if (randomnumber == "1") {
-    return "paper";
+    console.log("Computer: paper");
+    comp = "paper";
   } else if (randomnumber == "2") {
-    return "scissors";
+    console.log("Computer: scissors");
+    comp = "scissors";
   }
 }
 //user prompt
 //
-let user = prompt("Rock, Paper, Scissors?").toLowerCase();
+
+//let user = prompt("Rock, Paper, Scissors?").toLowerCase();
 //game function
 //
-function game(user, comp) {
-  comp = computerselect();
+function game() {
+  computerselect();
   if (user == comp) alert("you tie");
   else if (user == "rock" && comp == "paper") alert("you lose");
   else if (user == "rock" && comp == "scissors") alert("you win");
@@ -30,5 +56,3 @@ function game(user, comp) {
 }
 //run game
 //
-console.log(computerselect());
-game(user, comp);
