d84bb03818465e48f5e8b3b2eb7a7bc4a9af49f2
AsdWaring <a.s.d.waring@gmail.com>
2022-08-08 15:40:43 -0700

    created score board at button push

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # rock_paper_scissors
 
+Friday August 5th:
 Step 1:
 
 Create plan and outline of the structure of the web page.
@@ -7,7 +8,27 @@ Three image buttons rock paper scissors for user input
 
 Step 2:
 
-Write js script (no idea how to do this yet)
+Write js script (no idea how to do this yet) - I am learning Javascript from scratch.
+
 Current plan:
 Start with script for computer input for options
 Then user input
+
+Monday August 8th:
+
+Okay - so this has been a huge learning curve.
+I have rewritten my code numerous times and I have finally been
+able to have the game play on button click. It runs the game and tells
+the user if they win against the computer through alert.
+
+Btw - my steps above didn't work, I had to create script that
+consol.log'd the results for both user and computer seperately before
+combining the two to play it on the consol with an alert. THEN I was able
+to figure out how to use button.id and listeners to give the buttons functionality for the user.
+
+This has been a really great learning experience, and when I finally got the game
+to run on button click with the alert of who wins. I spontaniously laughed with joy.
+
+Next step:
+
+figure out how to play five rounds and have a counter of wins on the page.
--- random.js
+++ random.js
@@ -1,9 +1,9 @@
-/* I am making good progress, but I need to understand
-how to start the game with the the selection of the user buttons
-I am learning something new each time I tackle this problem 
-player selection may need to be converted into a function?*/
+/* got the game to work at button push, need to create a counter
+that displays who score and have it out of five games*/
 
 // setting constants
+let playerscorenumber = 0;
+let computerscorenumber = 0;
 const rockbutton = document.querySelector("#rock");
 const paperbutton = document.querySelector("#paper");
 const scissorsbutton = document.querySelector("#scissors");
@@ -45,17 +45,57 @@ function computerselect() {
 //
 
 //let user = prompt("Rock, Paper, Scissors?").toLowerCase();
-//game function
+//game function - I simpliefied the code using ||
 //
 function game() {
   computerselect();
-  if (user == comp) alert("you tie");
-  else if (user == "rock" && comp == "paper") alert("you lose");
-  else if (user == "rock" && comp == "scissors") alert("you win");
-  else if (user == "paper" && comp == "rock") alert("you win");
-  else if (user == "paper" && comp == "scissors") alert("you lose");
-  else if (user == "scissors" && comp == "rock") alert("you lose");
-  else if (user == "scissors" && comp == "paper") alert("you win");
+
+  if (user == comp) {
+    alert("you tie");
+    results =
+      "You tie <br></br>" +
+      user +
+      " ties " +
+      comp +
+      "<br></br>Player score: " +
+      playerscorenumber +
+      "<br></br>Computer Score: " +
+      computerscorenumber;
+  } else if (
+    (user == "rock" && comp == "paper") ||
+    (user == "paper" && comp == "scissors") ||
+    (user == "scissors" && comp == "rock")
+  ) {
+    alert("you lose");
+    computerscorenumber += 1;
+    results =
+      "You lose <br></br>" +
+      comp +
+      " beats " +
+      user +
+      "<br></br>Player score: " +
+      playerscorenumber +
+      "<br></br>Computer Score: " +
+      computerscorenumber;
+  } else if (
+    (user == "rock" && comp == "scissors") ||
+    (user == "paper" && comp == "rock") ||
+    (user == "scissors" && comp == "paper")
+  ) {
+    alert("you win");
+    playerscorenumber += 1;
+    results =
+      "You win <br></br>" +
+      user +
+      " beats " +
+      comp +
+      "<br></br>Player score: " +
+      playerscorenumber +
+      "<br></br>Computer Score: " +
+      computerscorenumber;
+  }
+  document.getElementById("results").innerHTML = results;
+  return;
 }
 //run game
 //
